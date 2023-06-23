b793cf0d3b23e81551ba78551aa66c1d95fb6a35
ertan3 <ernesttan96@gmail.com>
2023-06-22 21:29:15 -0700

    Styled Business index

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,45 +7,135 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-ApplicationRecord.transaction do 
-    puts "Destroying tables..."
-    # Unnecessary if using `rails db:seed:replant`
-    User.destroy_all
-  
-    puts "Resetting primary keys..."
-    # For easy testing, so that after seeding, the first `User` has `id` of 1
-    ApplicationRecord.connection.reset_pk_sequence!('users')
-  
-    puts "Creating users..."
-    # Create one user with an easy to remember username, email, and password:
-    User.create!(
-      first_name: 'John', 
-      last_name_initial: 'D',
-      email: 'demo@user.io', 
-      password: 'password'
-    )
-  
-    # More users
-    10.times do 
-      User.create!({
-        first_name: Faker::Name.first_name,
-        last_name_initial: Faker::Name.last_name[0],
-        email: Faker::Internet.unique.email,
-        password: 'password'
-      }) 
-    end
+puts "Destroying tables..."
+# Unnecessary if using `rails db:seed:replant`
+User.destroy_all
+Business.destroy_all
 
-    puts "Creating businesses..."
-    #Create businesses
-    10.times do
-      Business.create!({
-        name: Faker::Company.name,
-        address: Faker::Address.street_address,
-        latitude: Faker::Address.latitude,
-        longitude: Faker::Address.longitude,
-        category: Faker::Restaurant.type
-      })
-    end
+puts "Resetting primary keys..."
+# For easy testing, so that after seeding, the first `User` has `id` of 1
+ApplicationRecord.connection.reset_pk_sequence!('users')
 
-    puts "Done!"
-  end
+puts "Creating users..."
+
+User.create!(
+  first_name: 'John', 
+  last_name_initial: 'D',
+  email: 'demo@user.io', 
+  password: 'password'
+)
+
+puts "Created user!"
+
+puts "Creating Japanese Businesses..."
+#Create businesses
+Business.create!({
+  name: 'Sushi Go Crazy', 
+  address: '123 Main St.',
+  latitude: '37.791994', 
+  longitude: '-122.409425',
+  category: 'Japanese'
+})
+Business.create!({
+  name: 'Japantown Izakaya', 
+  address: '345 Japan St.',
+  latitude: '37.783592', 
+  longitude: '-122.425436',
+  category: 'Japanese'
+})
+Business.create!({
+  name: 'Omakase', 
+  address: '678 Noreiga St.',
+  latitude: '37.753680', 
+  longitude: '-122.487017',
+  category: 'Japanese'
+})
+Business.create!({
+  name: 'Ramenagi', 
+  address: '112 Van Ness St.',
+  latitude: '37.791036', 
+  longitude: '-122.422178',
+  category: 'Japanese'
+})
+Business.create!({
+  name: 'Tasty BoilingPot', 
+  address: '1111 Chinatown St.',
+  latitude: '37.793816', 
+  longitude: '-122.409548',
+  category: 'Japanese'
+})
+
+puts "Creating Viet Restaurants"
+
+Business.create!({
+  name: 'Pho 100', 
+  address: '100 Irving St.',
+  latitude: '37.763759', 
+  longitude: '-122.471272',
+  category: 'Vietnamese'
+})
+Business.create!({
+  name: 'Pho 200', 
+  address: '200 Irving St.',
+  latitude: '37.763954', 
+  longitude: '-122.471261',
+  category: 'Vietnamese'
+})
+Business.create!({
+  name: 'Bahn Me Likey', 
+  address: '999 Noriega St.',
+  latitude: '37.753779', 
+  longitude: '-122.484874',
+  category: 'Vietnamese'
+})
+Business.create!({
+  name: 'Spring Day', 
+  address: '999 Real St.',
+  latitude: '37.760460', 
+  longitude: '-122.438344',
+  category: 'Vietnamese'
+})
+Business.create!({
+  name: 'Golden Sun', 
+  address: '888 Sunny St.',
+  latitude: '37.757860', 
+  longitude: '-122.418900',
+  category: 'Vietnamese'
+})
+
+
+
+# puts "Attaching AWS images..."
+
+# aws_image_urls = [
+#   'https://pley1-seeds.s3.us-west-1.amazonaws.com/sushi1.jpeg',
+#   'https://pley1-seeds.s3.us-west-1.amazonaws.com/sushi2.jpg',
+#   'https://pley1-seeds.s3.us-west-1.amazonaws.com/sushi3.jpeg',
+# ]
+
+# res_1 = Business.find_by(id: '1')
+
+# if res_1.present?
+#   aws_image_urls.each_with_index do |image_url, index|
+#     res_1.photo.attach(
+#       io: URI.open(image_url),
+#       filename: "sushi#{index + 1}.#{image_url.split('.').last}"
+#     )
+#   end
+#   puts "AWS image successful"
+# else
+#   puts "Could not find business"
+# end
+
+# 9.times do
+#   Business.create!({
+#     name: Faker::Company.name,
+#     address: Faker::Address.street_address,
+#     latitude: Faker::Address.latitude,
+#     longitude: Faker::Address.longitude,
+#     category: Faker::Restaurant.type
+#   })
+# end
+
+
+puts "Done!"
--- frontend/src/components/BusinessIndex/index.js
+++ frontend/src/components/BusinessIndex/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom';
 import './BusinessIndex.css';
+import { BiMessage } from 'react-icons/bi'
 
 function BusinessIndex() {
     const [businesses, setBusinesses] = useState([]);
@@ -15,19 +16,33 @@ function BusinessIndex() {
     return (
         <div className="business-list">
             <h1>All Restaurants</h1>
-            <ul>
-                {businesses
-                    // .sort((a, b) => a.id - b.id)
-                    .map((business) => (
-                        <li key={business.id}>
-                            <Link to={`/businesses/${business.id}`}>
-                                <h2>{business.name}</h2>
-                            </Link>
-                            <p>Address: {business.address}</p>
-                            <p>Category: {business.category}</p>
-                        </li>
-                ))}
-            </ul>
+            <div className='business-index-container'>
+                <ul>
+                        {businesses.map((business, index) => (
+                        <Link to={`/businesses/${business.id}`} style={{textDecoration: 'none'}}>
+                        <div className='business-each'>
+                            <li key={business.id}>
+                                <div className='business-each-box'>
+                                    <div className='business-photo'>
+                                        <p>business photo goes here</p>
+                                    </div>
+                                    <div className='business-info'>
+                                        <h2 className='business-header-font'>{index + 1}. <span className='name-hover'>{business.name}</span></h2>
+                                        <p className='ratings-row'>Ratings go here</p>
+                                        <p className='dollar-row'><button className='category-font'>{business.category}</button> $$</p>
+                                        <p className='open-row'>Open until 9:00 PM</p>
+                                        <div className='review-preview-container'>
+                                            <p className='text-icon'><BiMessage/></p>
+                                            <p className='review-preview-text'>"Reviews go here. Test test test test test test etst est testing testing testing testing testing testing testing testing testing testing testing test..." <span className='more-text'>more</span></p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </div>
+                        </Link>
+                        ))}
+                </ul>
+            </div>
         </div>
     );
 }
