9cc6fefd64effc56690d91948c212bd402561b88
ertan3 <ernesttan96@gmail.com>
2023-06-23 00:32:52 -0700

    Completed review controller

---
+++ app/controllers/api/reviews_controller.rb
@@ -0,0 +1,43 @@
+class Api::ReviewsController < ApplicationController
+    before_action :require_logged_in, only: [:create, :update, :destroy]
+
+    def index
+        @reviews = Review.all
+        render json: @reviews
+    end
+
+    def show
+        @review = Review.find(params[:id])
+        render json: @review
+    end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            render json: @review
+        else
+            render json: @review.errors, status: 422
+        end
+    end
+
+    def update
+        @review = Review.find(params[:id])
+        if @review.update(review_params)
+            render json: @review
+        else
+            render json: @review.errors, status: 422
+        end
+    end
+
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:body, :rating, :user_id, :business_id)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
     resources :businesses, only: [:index, :show]
+    resources :reviews, only: [:index, :show, :create, :update, :destroy]
   end
 
   get '*path', to: "static_pages#frontend_index"
